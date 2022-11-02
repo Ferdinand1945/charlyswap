c3da35db6a2eb16a750604016bc7be1153647905
Fernando Lopez <fernando.lopez@learnifier.com>
2022-11-02 12:47:55 +0100

    first commit - completed project

---
+++ packages/react-app/src/assets/index.js
@@ -0,0 +1,5 @@
+import chevronDown from "./chevron-down.svg";
+import ethereumLogo from "./ethereumLogo.png";
+import uniswapLogo from "./logo_charlyswap.png";
+
+export { chevronDown, ethereumLogo, uniswapLogo };
